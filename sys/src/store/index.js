93c36816c8bb948364e7a8d2a6226a026a71104e
wxamlzm <zd5024@163.com>
2021-10-13 19:15:32 +0800

    UPDATE:vuex模块化尝试成功

--- sys/src/store/index.js
+++ sys/src/store/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import state from './state'
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        name: '张三'
+    state,
+    mutations: {
+
     },
+    actions: {
+        
+    }
 });
 
 export default store;
