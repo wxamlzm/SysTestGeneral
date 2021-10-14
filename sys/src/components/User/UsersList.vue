fad1f945e9ba18b38972790285e4236978cb603e
wxamlzm <zd5024@163.com>
2021-10-14 16:43:45 +0800

    UPDATE:更新用户列表页面

--- sys/src/components/User/UsersList.vue
+++ sys/src/components/User/UsersList.vue
@@ -7,6 +7,24 @@
             <el-breadcrumb-item>用户列表</el-breadcrumb-item>
         </el-breadcrumb>
         <!-- 导航区域结束 -->
+       
+        <!-- 卡片视图开始 -->
+        <el-card>
+            <!-- 搜索区域开始 -->
+            <el-row :gutter="20">
+                <el-col :span="7">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary">添加用户</el-button>
+                </el-col>
+            </el-row>
+
+            <!-- 搜索区域结束 -->
+        </el-card>
+        <!-- 卡片视图结束 -->
     </div>
 </template>
 
@@ -15,3 +33,15 @@ export default {
     name: 'userslist'
 }
 </script>
+
+<style lang="scss">
+.users-list {
+    .el-breadcrumb {
+        margin-bottom: 15px;
+        font-size: 12px;
+    }
+    .el-card {
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
+    }
+}
+</style>
--- sys/src/views/Home/HomeAside.vue
+++ sys/src/views/Home/HomeAside.vue
@@ -69,7 +69,6 @@ export default {
     },
     mounted(){
         this.getMenuList();
-
     }
 }
 </script>
